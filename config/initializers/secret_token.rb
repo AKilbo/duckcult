c34540ec7129861e5b8b30360a2c3c454ffb0964
Alex Kilbo <akilbo@gmail.com>
2013-02-22 14:44:46 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duckcult::Application.config.secret_token = 'a281d4e79b8b89f4dd8dc45b03508bc54947f8c27c7a240354aca60e6c1b9e93dd308cf75e301c4a35a6737a9d3212834f731a4710de9d0ca1a712b762c9d9c0'
